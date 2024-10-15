b62c52ef2c07640d3937e10cb6efac89c5ac81aa
Musa Sayar <musasayar67@gmail.com>
2024-10-15 13:01:14 +0300

    work styles

--- code/01-working-with-core-components/App.js
+++ code/01-working-with-core-components/App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another piece of text</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello worlds </Text>
+      <Button color={"red"} title="Click me" />
     </View>
   );
 }
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "#4b00ff",
+    padding: 5,
+    fontSize: 24,
+    textAlign: "center",
+    borderRadius: 12,
   },
 });
