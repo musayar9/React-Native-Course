98736f4d9644b17a6beb47e696971a38be8cc210
Musa Sayar <musasayar67@gmail.com>
2024-10-24 18:02:28 +0300

    edit start game screen

--- code/04-deep-real-app/App.js
+++ code/04-deep-real-app/App.js
@@ -1,13 +1,12 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+
             <StartGameScreen/>
-            <StatusBar style="auto"/>
+
         </View>
     );
 }
@@ -15,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+
     },
 });
--- code/04-deep-real-app/screens/StartGameScreen.js
+++ code/04-deep-real-app/screens/StartGameScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {TextInput, View} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
 
             <TextInput/>
             <PrimaryButton>Reset</PrimaryButton>
@@ -13,4 +13,27 @@ const StartGameScreen = () => {
     );
 }
 
+
 export default StartGameScreen;
+
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        // flex: 1,
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: "#72063c",
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+
+    }
+
+})
